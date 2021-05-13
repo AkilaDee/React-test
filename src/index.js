a61729bd06277dd183568ef841b7e67bce49c20a
Akila De Silva <akiladesilva97@gmail.com>
2021-05-13 15:46:00 +0530

    react

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+const element=<h1 className="testclass">Welcome to uso penetensary</h1>;
+
+ReactDOM.render(element,document.getElementById("root"));
+
+
+const newElement=<h1 className="testclass">brave</h1>;
+
+ReactDOM.render(newElement,document.getElementById("root1"));
